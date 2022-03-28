19325a4a8a51e37943d8e6b749bfe56bed280667
LindAchkA <100114796+LindAchkA@users.noreply.github.com>
2022-03-28 09:05:52 +0300

    123
    123

--- server.py
+++ server.py
@@ -10,14 +10,19 @@ def root():
     return render_template("index.html")
 
 @app.route('/about')    
-def-about():
+def about():
   return render_template("about.html")
-  
-      
+
 #Pārbaudes lapa, lai saprastu, ka kods vispār strādā
 @app.route('/health')
 def health():
   return "OK"
+
+@app.route('/test',methods = ['POST', 'GET'])
+def tests():
+    parametri = ["IQ","Augums","Kājas izmērs"]
+    return render_template("test.html", parametri=parametri)
+ 
 
 if __name__ == '__main__':
   app.run(debug="true")
